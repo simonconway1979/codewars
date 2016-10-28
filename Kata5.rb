e5dcdccd7806b98a4566e3c04af45a3cb1079a9b
Simon Conway <simon.conway@condaal.com>
2016-10-28 04:06:47 +0100

    Kata 5 - change vowels into 1s and other chars into 0

---
+++ Kata5.rb
@@ -0,0 +1,10 @@
+#Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
+
+#All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+
+
+def vowel_one(s)
+    # your code here
+
+    return ''
+end
