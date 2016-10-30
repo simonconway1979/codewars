a0f8661d63a12fdea9ec960833d5315dd9bcbe37
Simon Conway <simon.conway@condaal.com>
2016-10-30 09:47:05 +0000

    Kata9 start

---
+++ Kata9.rb
@@ -0,0 +1,17 @@
+# Description:
+
+#Positive integers that are divisible exactly by the sum of their digits are called Harshad numbers, see Wikipedia for further details.
+
+#Implement the method isSortedAndHow, which accepts an array of integers, and returns one of the following:
+
+#'yes, ascending' - if the numbers in the array are sorted in an ascending way
+#'yes, descending' - if the numbers in the array are sorted in a descending way
+#'no'
+#You can assume the array will always be valid, and there will always be one correct answer.
+
+
+
+def is_sorted_and_how(arr)
+    # your code here
+    return ''
+end
