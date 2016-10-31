2777a0d4007b25cfeb880182b83400d55e03e320
Simon Conway <simon.conway@condaal.com>
2016-10-31 15:08:52 +0000

    Kata17 start

---
+++ Kata17.rb
@@ -0,0 +1,21 @@
+#Say hello!
+
+#Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null/nil if input is empty string or null/nil.
+
+#Example:
+
+#greet("Niks") == "hello Niks!"
+#greet("") == nil; # Return nil if input is empty string
+#greet(nil) == nil; # Return nil if input is nil
+
+
+def greet(name)
+  if greet == "" || greet ==  nil
+  return nil
+  else
+  return "test"
+  end
+
+end
+
+def greet("")
