6eb8fb5cec7591f40350a3761ba030dd60d8bc48
Simon Conway <simon.conway@condaal.com>
2016-10-31 15:08:22 +0000

    Kata16 failed

--- Kata16.rb
+++ Kata16.rb
@@ -1,35 +1,21 @@
-#Our fruit guy has a bag of fruits (array of strings) where some fruits are rotten, he wants to replace all the rotten fruits by good ones. For example, given this array ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that will take as an argument an array of strings containing fruits and should return an array of strings where all the rotten fruits are replaced by good ones.
+#Say hello!
 
-#Notes
+#Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null/nil if input is empty string or null/nil.
 
-#If the array is null or empty you should return empty array ([]).
-#The rotten fruit name will be in this format rottenFruitname where is the 1st letter of the fruit name is uppercase.
+#Example:
 
-#NB
+#greet("Niks") == "hello Niks!"
+#greet("") == nil; # Return nil if input is empty string
+#greet(nil) == nil; # Return nil if input is nil
 
-#The returned array should be in LOWER case.
 
-
-def remove_rotten(fruitBasket) 
-
-  fruitBasket = @fruitBasket
-
-  #rescue ArgumentError
-#remove_rotten([])  
-#end
- 
-def fixed_fruit
-  @fixed_fruit = @fruitBasket.each.with_index do |a,b|
-  @fruitBasket[b] = a.downcase.gsub('rotten','')
+def greet(name)
+  if greet == "" || greet ==  nil
+  return nil
+  else
+  return "test"
   end
 
-  p @fixed_fruit
-
 end
 
-fixed_fruit
-
-end
-
-remove_rotten(["apple", "rottenBanana"])
-
+def greet("")
