200a7cf8dc3b768d67ecbfab6b89a357c83817ed
Simon Conway <simon.conway@condaal.com>
2016-10-31 12:31:27 +0000

    Kata11 not complete (Poor instructions.)

--- Kata11.rb
+++ Kata11.rb
@@ -5,5 +5,17 @@
 
 
 def multiples(x)
-  #Go on
+  if x%7 == 0 
+    puts "Fizz"
+  elsif x%49 == 0 && x%3 == 0
+    puts "Fang"
+  elsif x%15 == 0
+    puts "Foo"
+  else 
+    puts "Far"
+  end
 end
+
+
+
+multiples(147)
