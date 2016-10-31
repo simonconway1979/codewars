a28de63022d9247f9732e89b7d4aa74fca590dce
Simon Conway <simon.conway@condaal.com>
2016-10-31 12:58:47 +0000

    Kata13 start

---
+++ Kata13.rb
@@ -0,0 +1,10 @@
+# Hello! Today your task is to build a basic die feature, where you will get a range 
+
+#in the form (min, max) - both included - and return a random number in the inclusive range.
+
+#Props if you don't use your language's random library!
+
+
+def dice(minimum, maximum)
+  #your code here
+end
